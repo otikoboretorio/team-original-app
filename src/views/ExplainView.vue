55681fc901a2efefa7884d1c0f314fb5eb135dbc
ginozakimchura <ginozakim@gmail.com>
2022-12-11 17:56:50 +0900

    テンプレート少し書いてみた

---
+++ src/views/ExplainView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">解説やで！！！</div>
+  <div class="hello">楽しく学ぼう😊</div>
+  <ul>
+    <li>ビール</li>
+    <li>のど越しがいい</li>
+    <li>乾杯の合図</li>
+    <li>おいしい</li>
+  </ul>
+  <button v-on:click="change">次へ</button>
+</template>
+
+<style>
+.hello {
+  color: blue;
+}
+</style>
+
+<script></script>
