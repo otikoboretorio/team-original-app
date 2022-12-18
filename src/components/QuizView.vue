c25cb48bcd6b99d01dc5d40a73dbf5c5a1731ae7
kouphasi <phasi.ok.phasi@gmail.com>
2022-12-18 19:29:32 +0900

    props適応完成

---
+++ src/components/QuizView.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>{{ question }}</h1>
+    <div v-for="( selection, index) in selections" v-bind:key="selection">
+        <button @click="show">
+            <div>
+                {{ index }}.{{ selection }}
+            </div>
+        </button>
+    </div>
+    <div v-if="isshow">
+        <ExplainView :star_num="explain.star_num" :ans="explain.ans" :details="explain.details"></ExplainView>
+    </div>
+</template>
+
+<script>
+import ExplainView from '@/components/ExplainView.vue';
+
+export default {
+    props: ["question", "selections", "answer", "explain"],
+    components: {
+        ExplainView
+    },
+    data() {
+        return {
+            isshow: false
+        }
+    },
+    methods: {
+        show() {
+            this.isshow = true
+        },
+        notshow() {
+            this.isshow = false
+        }
+    },
+    created() {
+        console.log(this.explain.star_num)
+    },
+}
+</script>
---
+++ src/views/QuizPage.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <QuizView :question="question.sentence" :selections="question.selections" :answer="question.answer"
+            :explain="question.explain"></QuizView>
+        <ExplainView></ExplainView>
+    </div>
+    <button @click="nextPage">次へ</button>
+</template>
+  
+<script>
+import QuizView from '@/components/QuizView.vue';
+export default {
+    components: {
+        QuizView
+    },
+    data() {
+        return {
+            quiz: [{
+                sentence: "日本酒の主な材料はなんでしょうか？",
+                selections: ["お米", "ブドウ", "小麦"],
+                answer: 1,
+                explain: {
+                    star_num: 1,
+                    ans: "お米",
+                    details: "日本酒は、米と麹（こうじ）と水が材料となっています。"
+                }
+            },
+            {
+                sentence: "洋酒の主な材料はなんでしょうか？",
+                selections: ["お米", "ブドウ", "小麦"],
+                answer: 1,
+                explain: {
+                    star_num: 1,
+                    ans: "お米",
+                    details: "日本酒は、米と麹（こうじ）と水が材料となっています。"
+                }
+            }
+            ],
+            question_number: 0,
+        }
+    },
+    methods: {
+        nextPage() {
+            if (this.question_number < this.quiz.length - 1) {
+                this.question_number += 1
+            } else {
+                this.question_number = 0
+            }
+        }
+    },
+    computed: {
+        question() {
+            return this.quiz[this.question_number]
+        }
+    }
+}
+</script>
