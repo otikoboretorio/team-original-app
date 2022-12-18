0db8d80e80bcc621036eabc1b63bd435966307dd
kouphasi <phasi.ok.phasi@gmail.com>
2022-12-18 19:45:56 +0900

    allow changepage

--- src/components/QuizView.vue
+++ src/components/QuizView.vue
@@ -1,6 +1,6 @@
 <template>
-    <h1>{{ question }}</h1>
-    <div v-for="( selection, index) in selections" v-bind:key="selection">
+    <h1>{{ question.sentence }}</h1>
+    <div v-for="( selection, index) in question.selections" v-bind:key="selection">
         <button @click="show">
             <div>
                 {{ index }}.{{ selection }}
@@ -8,21 +8,45 @@
         </button>
     </div>
     <div v-if="isshow">
-        <ExplainView :star_num="explain.star_num" :ans="explain.ans" :details="explain.details"></ExplainView>
+        <ExplainView :star_num="question.explain.star_num" :ans="question.explain.ans"
+            :details="question.explain.details"></ExplainView>
     </div>
+    <button @click="nextPage" :disabled="!isshow">次へ</button>
 </template>
 
 <script>
 import ExplainView from '@/components/ExplainView.vue';
 
 export default {
-    props: ["question", "selections", "answer", "explain"],
+    // props: ["question", "selections", "answer", "explain"],
     components: {
         ExplainView
     },
     data() {
         return {
-            isshow: false
+            isshow: false,
+            quiz: [{
+                sentence: "日本酒の主な材料はなんでしょうか？",
+                selections: ["お米", "ブドウ", "小麦"],
+                answer: 1,
+                explain: {
+                    star_num: 1,
+                    ans: "お米",
+                    details: "日本酒は、米と麹（こうじ）と水が材料となっています。"
+                }
+            },
+            {
+                sentence: "洋酒の主な材料はなんでしょうか？",
+                selections: ["お米", "ブドウ", "小麦"],
+                answer: 1,
+                explain: {
+                    star_num: 1,
+                    ans: "お米",
+                    details: "日本酒は、米と麹（こうじ）と水が材料となっています。"
+                }
+            }
+            ],
+            question_number: 0,
         }
     },
     methods: {
@@ -31,10 +55,20 @@ export default {
         },
         notshow() {
             this.isshow = false
+        },
+        nextPage() {
+            this.notshow()
+            if (this.question_number < this.quiz.length - 1) {
+                this.question_number += 1
+            } else {
+                this.question_number = 0
+            }
         }
     },
-    created() {
-        console.log(this.explain.star_num)
-    },
+    computed: {
+        question() {
+            return this.quiz[this.question_number]
+        }
+    }
 }
 </script>
